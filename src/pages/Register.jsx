1055c10de7824cb6b92420c364bcc181be3167c0
Franco Anfossi <franco.anfossi@uc.cl>
2024-04-09 01:47:42 -0400

    change: register endpoint

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,7 @@ import UserForm from "../components/UserForm";
 
 function Register() {
     return (
-        <UserForm endpoint="/api/users/register/" method="register" />
+        <UserForm endpoint="/api/users/register/company/" method="register" />
     );
 }
 
